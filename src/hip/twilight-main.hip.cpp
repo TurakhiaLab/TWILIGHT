40e1f5bfe49d98fa8255910136ffc37dde758435
root <y3tseng@ucsd.edu>
2025-08-21 20:09:46 +0000

    update hip

--- src/hip/twilight-main.hip.cpp
+++ src/hip/twilight-main.hip.cpp
@@ -10,48 +10,74 @@
 
 #include "../version.hpp"
 
-po::options_description mainDesc("TWILIGHT Command Line Arguments");
+po::options_description mainDesc("TWILIGHT Command Line Arguments", 120);
+
+
+    
 
 void parseArguments(int argc, char** argv)
 {
-    // Setup boost::program_options
-    mainDesc.add_options()
-        ("tree,t", po::value<std::string>(), "Guide Tree - Newick format (required if building MSA from raw sequences).")
-        ("sequences,i", po::value<std::string>(), "Input tip sequences - Fasta format (required if building MSA from raw sequences).")
-        ("files,f", po::value<std::string>(), "Path to the directory containing all MSA files. MSA files - Fasta format.")
+    
+    // Section: I/O
+    po::options_description inputDesc("Inputs\n [1] Build MSA From Unaligned Sequences\n [2] Merge Multiple MSAs\n [3] Add New Sequences to Existing MSA");
+    inputDesc.add_options()
+        ("tree,t", po::value<std::string>(), "Guide tree (Newick format): required for [1]; optional for [3].")
+        ("sequences,i", po::value<std::string>(), "Unaligned sequences file (FASTA format): required for [1] and [3].")
+        ("alignment,a", po::value<std::string>(), "Backbone alignments (FASTA format): required for [3].")
+        ("files,f", po::value<std::string>(), "Directory containing all MSA files. MSA files (FASTA format): required for [2].");
+   
+    po::options_description outputDesc("Output/File Options");
+    outputDesc.add_options()
         ("output,o", po::value<std::string>(), "Output file name (required).")
-        ("cpu,C",  po::value<int>(), "Number of CPU cores. Default: all available cores.")
-        ("gpu,G",  po::value<int>(), "Number of GPUs. Default: all available GPUs.")
-        ("gpu-index", po::value<std::string>(), "Specify the GPU to be used, separated by commas. Ex. 0,2,3")
-        ("max-subtree,m", po::value<int>(), "Maximum number of leaves in a subtree.")
-        ("max-group,g",  po::value<int>(), "Maximum number of leaves in a group within a subtree, used for performing transitivity merger in a subtree.")
         ("temp-dir,d", po::value<std::string>(), "Directory for storing temporary files.")
-        ("remove-gappy,r", po::value<float>()->default_value(0.95), "Threshold for removing gappy columns. Set to 1 to disable this feature.")
-        ("type", po::value<std::string>(), "Data type. n: nucleotide, p: protein. Will be automatically inferred if not provided.")
-        // ("gappy-horizon,z", po::value<float>()->default_value(1), "Minimum number of consecutive gappy columns, which will be removed during alignment.")
-        ("wildcard,w", "Treat unknown or ambiguous bases as wildcards and align them to usual letters.")
-        ("verbose,v", "Print out every detail process.")
-        ("psgop,p", po::value<std::string>()->default_value("y"), "y: Enable, n: Disable position-specific gap open penalty.")
-        ("match",      po::value<float>()->default_value(18), "Match score.")
-        ("mismatch",   po::value<float>()->default_value(-8), "Mismatch penalty for transversions.")
-        ("transition", po::value<float>()->default_value(-4), "Score for transitions")
-        ("gap-open",   po::value<float>()->default_value(-50), "Gap-Open penalty")
-        ("gap-extend", po::value<float>()->default_value(-5), "Gap-Extend penalty")
-        ("gap-ends",   po::value<float>(), "Gap penalty at ends, default set to the same as the gap extension penalty.")
-        ("xdrop",      po::value<float>()->default_value(600), "X-drop value (scale). The actual X-drop will be multiplied by the gap-extend penalty.")
-        ("matrix,x", po::value<std::string>(), "Use a user-defined substitution matrix.")
-        ("output-type", po::value<std::string>()->default_value("FASTA"), "FASTA or CIGAR, CIGAR stands for CIGAR-like compressed format.")
-        ("merge", po::value<std::string>()->default_value("p"), "t: Transitivity merger, p: Progressive alignment.")
         ("keep-temp,k", "Keep the temporary directory.")
-        ("sum-of-pairs-score,s", "Calculate the sum-of-pairs score after the alignment.")
+        ("overwrite", "Force overwriting the output file.")
+        ("write-prune", "Write the pruned tree to the output directory.");
+
+    // Section: Hardware
+    po::options_description hardwareDesc("Hardware Usage");
+    hardwareDesc.add_options()
+        ("cpu,C", po::value<int>(), "Number of CPU cores. Default: all available cores.")
+        ("gpu,G", po::value<int>(), "Number of GPUs. Default: all available GPUs.")
+        ("gpu-index", po::value<std::string>(), "Specify the GPU to be used, separated by commas. Ex. 0,2,3")
+        ("cpu-only", "Run the program only on CPU.");
+
+    // Section: Alignment Parameters
+    po::options_description alignDesc("Alignment Options and Parameters");
+    alignDesc.add_options()
+        ("type", po::value<std::string>(), "Data type. n: nucleotide, p: protein. Will be automatically inferred if not provided.")
+        ("max-subtree,m", po::value<int>(), "Maximum number of leaves in a subtree.")
+        ("remove-gappy,r", po::value<float>()->default_value(0.95), "Threshold for removing gappy columns. Set to 1 to disable this feature.")
+        ("prune", "Prune the input guide tree based on the presence of unaligned sequences.")
+        ("wildcard,w", "Treat unknown or ambiguous bases as wildcards and align them to usual letters.")
         ("no-align-gappy", "Do not align gappy columns. This will create a longer MSA (larger file).")
+        // ("psgop", po::value<std::string>()->default_value("y"), "y: Enable, n: Disable position-specific gap open penalty.")
         ("length-deviation", po::value<float>(), "Sequences whose lengths deviate from the average by more than the specified fraction will be deferred or excluded.")
         ("max-ambig", po::value<float>()->default_value(0.1), "Sequences with an ambiguous character proportion exceeding the specified threshold will be deferred or excluded.")
         ("filter", "Exclude sequences with high ambiguity or length deviation.")
+        ("merge", po::value<std::string>()->default_value("p"), "t: Transitivity merger, p: Progressive alignment.");
+        
+    // Section: Scoring Parameters
+    po::options_description scoringDesc("Scoring Parameters");
+    scoringDesc.add_options()
+        ("match", po::value<float>()->default_value(18), "Match score.")
+        ("mismatch", po::value<float>()->default_value(-8), "Mismatch penalty for transversions.")
+        ("transition", po::value<float>()->default_value(-4), "Score for transitions.")
+        ("gap-open", po::value<float>()->default_value(-50), "Gap-Open penalty.")
+        ("gap-extend", po::value<float>()->default_value(-5), "Gap-Extend penalty.")
+        ("gap-ends", po::value<float>(), "Gap penalty at ends, default set to the same as the gap extension penalty.")
+        ("xdrop", po::value<float>()->default_value(600), "X-drop value (scale). The actual X-drop will be multiplied by the gap-extend penalty.")
+        ("matrix,x", po::value<std::string>(), "Use a user-defined substitution matrix.");        
+
+    po::options_description generalDesc("General");
+    generalDesc.add_options()
         ("check", "Check the final alignment. Sequences with no legal alignment will be displayed.")
-        ("cpu-only", "Run the program only on CPU.")
-        ("help,h", "Print help messages")
-        ("version", "Show program version");
+        ("verbose,v", "Print out every detail process.")
+        ("help,h", "Print help messages.")
+        ("version", "Show program version.");
+
+    // Setup boost::program_options
+    mainDesc.add(inputDesc).add(outputDesc).add(hardwareDesc).add(alignDesc).add(scoringDesc).add(generalDesc);
 }
 
 int main(int argc, char** argv) {
@@ -65,6 +91,7 @@ int main(int argc, char** argv) {
         po::notify(vm);
     }
     catch(std::exception &e){
+        std::cerr << "Error: " << e.what() << std::endl;
         std::cerr << mainDesc << std::endl;
         if(vm.count("help"))
             return 0;
@@ -93,6 +120,11 @@ int main(int argc, char** argv) {
     if (option->alnMode == 0) { // Twilight
         // Partition tree into subtrees
         T = readNewick(option->treeFile);
+        if (vm.count("prune")) {
+            T = pruneTree(T, option->seqFile);
+            if (vm.count("write-prune")) outputPrunedTree(T, option);
+        }
+        // printTree(T->root, -1);
         P = new partitionInfo_t(option->maxSubtree, 0, 0, "centroid"); 
         partitionTree(T->root, P);
         newT = reconsturctTree(T->root, P->partitionsRoot);
@@ -159,7 +191,7 @@ int main(int argc, char** argv) {
                 }
                 if (P->partitionsRoot.size() > 1) {
                     auto storeStart = std::chrono::high_resolution_clock::now();
-                    storeFreq(util, subT, subtree);
+                    storeFreq(util, option, subT, subtree);
                     outputSubAln(util, option, subT, subtree);
                     // util->storeCIGAR();
                     util->seqsFree();
@@ -208,7 +240,7 @@ int main(int argc, char** argv) {
             std::cout << "Output " << newT->allNodes.size() << " alignments (total " << totalSeqs << " sequences) in " << outTime.count() / 1000000 << " ms\n";
         }
     }
-    else { // Twilight-Mer
+    else if (option->alnMode == 1) { // Twilight-Mer
         readFrequency(util, option);
         T = new Tree(util->seqsLen, util->seqsIdx);
         util->nowProcess = 2; // merge subtrees
@@ -225,6 +257,96 @@ int main(int argc, char** argv) {
         auto outEnd = std::chrono::high_resolution_clock::now();
         std::chrono::nanoseconds outTime = outEnd - outStart;
         std::cout << "Output " << T->allNodes.size() - 1 << " MSAs (total " << totalSeqs << " sequences) in " << outTime.count() / 1000000 << " ms\n";
+    }
+    else if (option->alnMode == 2) { // Add new sequences
+        if (option->treeFile == "") {
+            T = new Tree();
+            readMSA_and_Seqs(util, option, T);
+            P = new partitionInfo_t(INT32_MAX, 0, 0, "centroid");
+            option->nowProcess = 0;
+            partitionTree(T->root, P);
+        }
+        else {
+            T = readNewick(option->treeFile);
+            P = new partitionInfo_t(option->maxSubtree, 0, 0, "centroid"); 
+            option->nowProcess = 0;
+            partitionTree(T->root, P);
+            newT = reconsturctTree(T->root, P->partitionsRoot);
+            if (P->partitionsRoot.size() > 1) {
+                std::cout << "Decomposed the tree into " << P->partitionsRoot.size() << " subtrees.\n";
+                outputSubtreeTrees(T, P, util, option);
+            }
+        }
+        int proceeded = 0;
+        auto alnSubtreeStart = std::chrono::high_resolution_clock::now();
+        for (auto subRoot: P->partitionsRoot) {
+            auto subtreeStart = std::chrono::high_resolution_clock::now();
+            ++proceeded;
+            int subtree = T->allNodes[subRoot.first]->grpID;
+            if (P->partitionsRoot.size() > 1) std::cout << "Start processing subalignment No. " << subtree << ". (" << proceeded << '/' << P->partitionsRoot.size() << ")\n";
+            Tree* subT = new Tree(subRoot.second.first);
+            if (option->treeFile != "") {
+                readBackboneAln(util, option, subT);
+                readNewSequences(util, option, subT);
+            }
+            else {
+                subT->root->msaFreq = T->root->msaFreq;
+                subT->root->msa = T->root->msa;
+            }
+            auto treeBuiltStart = std::chrono::high_resolution_clock::now();
+            partitionInfo_t * subP = new partitionInfo_t(INT32_MAX, 0, 0, "centroid");
+            partitionTree(subT->root, subP);
+            auto treeBuiltEnd = std::chrono::high_resolution_clock::now();
+            std::chrono::nanoseconds treeBuiltTime = treeBuiltEnd - treeBuiltStart;
+            placementGpu(subT, util, option, subP, *param);
+            if (P->partitionsRoot.size() > 1) {
+                auto storeStart = std::chrono::high_resolution_clock::now();
+                storeFreq(util, option, subT, subtree);
+                outputSubAln(util, option, subT, subtree);
+                util->clearAll();
+                auto storeEnd = std::chrono::high_resolution_clock::now();
+                std::chrono::nanoseconds storeTime = storeEnd - storeStart;
+                std::cout << "Stored the subalignments in " << storeTime.count() / 1000000 << " ms.\n";
+            }
+            if (option->treeFile == "") T->root->msaAln = subT->root->msaAln;
+            delete subP;
+            delete subT;
+            auto subtreeEnd = std::chrono::high_resolution_clock::now();
+            std::chrono::nanoseconds subtreeTime = subtreeEnd - subtreeStart;
+            if (P->partitionsRoot.size() > 1) std::cout << "Finished subalignment No." << subtree << " in " << subtreeTime.count() / 1000000000 << " s\n";
+            else                              std::cout << "Finished the alignment in " << subtreeTime.count() / 1000000000 << " s\n";
+        }
+        if (P->partitionsRoot.size() > 1) {
+            auto alnSubtreeEnd = std::chrono::high_resolution_clock::now();
+            std::chrono::nanoseconds alnSubtreeTime = alnSubtreeEnd - alnSubtreeStart;
+            std::cout << "Finsihed all subalignments in " << alnSubtreeTime.count() / 1000000000 << " s.\n";
+        }
+        if (P->partitionsRoot.size() > 1) {
+            util->nowProcess = 2; // merge subtrees
+            option->alnMode = 0;
+            updateSeqLen(T, P, util);
+            if (option->merger == "transitivity") {
+                auto alnStart = std::chrono::high_resolution_clock::now();
+                alignSubtrees (T, newT, util, option, *param);
+                mergeSubtrees (T, newT, util, option, *param);
+                auto alnEnd = std::chrono::high_resolution_clock::now();
+                std::chrono::nanoseconds alnTime = alnEnd - alnStart;
+                std::cout << "Merge " << newT->allNodes.size() << " subalignments in " << alnTime.count() / 1000000 << " ms\n";
+            }
+            else if (option->merger == "profile") {
+                auto alnStart = std::chrono::high_resolution_clock::now();
+                mergeSubtrees (T, newT, util, option, *param);
+                auto alnEnd = std::chrono::high_resolution_clock::now();
+                std::chrono::nanoseconds alnTime = alnEnd - alnStart;
+                std::cout << "Progressive alignment on " << newT->allNodes.size() << " subalignments in " << alnTime.count() / 1000000 << " ms\n";
+            }
+            int totalSeqs = 0;
+            auto outStart = std::chrono::high_resolution_clock::now();
+            outputFinal (T, P, util, option, totalSeqs);
+            auto outEnd = std::chrono::high_resolution_clock::now();
+            std::chrono::nanoseconds outTime = outEnd - outStart;
+            std::cout << "Output " << newT->allNodes.size() << " alignments (total " << totalSeqs << " sequences) in " << outTime.count() / 1000000 << " ms\n";
+        }
     }
     
     
